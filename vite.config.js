9c1f187cf97c21726f956ee47145e21b6ec978a5
Rashid Amin <rashidaminbuhroo@gmail.com>
2026-06-13 13:32:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// System Reset Configuration
 export default defineConfig({
   plugins: [react()],
-  base: './', // Using relative paths forces the browser to find files wherever they are
+  base: '/sahil-cloud-engineer/'
 })
