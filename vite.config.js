b18ed22c144e18ef5ca99928dbd6217f058091ba
Rashid Amin <rashidaminbuhroo@gmail.com>
2026-06-13 13:12:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/sahil-cloud-engineer/',
+  build: {
+    outDir: '.', // Forces Vite to build files right into the main directory
+  }
 })
